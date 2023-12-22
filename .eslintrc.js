c1f7976e56943e89a50e1616dd7a5ad34b5fc770
Chris <chrisgithub2682@gmail.com>
2023-12-22 18:07:04 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@babel/eslint-parser',
+    plugins: ['react', 'react-native'],
+    env: {
+        'react-native/react-native': true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all', 'plugin:react/jsx-runtime'],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
+}
