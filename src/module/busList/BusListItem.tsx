7190e3429637e1491d1ec9de348a534a30b16c08
Chris <chrisgithub2682@gmail.com>
2023-12-24 16:02:47 +0800

    OFFT-003 add i18n support for bus list page

--- src/module/busList/BusListItem.tsx
+++ src/module/busList/BusListItem.tsx
@@ -2,30 +2,41 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 import { getEtaMinuteByDate } from '../../util/DateUtil'
 
-const BusListItem = ({data, onItemClick}) => (
-  <TouchableOpacity style={{ flex: 1 }} onPress={() => onItemClick(data)}>
-    <View style={styles.container}>
-      <Text style={styles.route}>{data['route']}</Text>
-      <View style={[styles.column_container, styles.destination_container]}>
-        <View style={[styles.column_container, styles.row_container]}>
-          <Text style={styles.destination_label}>往</Text>
-          <Text style={styles.destination}>{data['dest_tc']}</Text>
+import { useTranslation } from "react-i18next"
+import { getCurrentLocale } from '../../util/LanguageUtil'
+
+const BusListItem = ({data, onItemClick}) => {
+  
+  const { t } = useTranslation()
+  const originStop = getCurrentLocale() == "en" ? data['origin_stop']['name_en'] : data['origin_stop']['name_tc']
+  const dest_tc = getCurrentLocale() == "en" ? data['dest_en'] : data['dest_tc']
+
+  return(
+    <TouchableOpacity style={{ flex: 1 }} onPress={() => onItemClick(data)}>
+      <View style={styles.container}>
+        <Text style={styles.route}>{data['route']}</Text>
+        <View style={[styles.column_container, styles.destination_container]}>
+          <View style={[styles.row_container]}>
+            <Text style={styles.destination_label}>{t('destination_label')}</Text>
+            <Text style={styles.destination}>{dest_tc}</Text>
+          </View>
+          <Text style={styles.origin_stop}>{originStop}</Text>
         </View>
-        <Text style={styles.origin_stop}>{data['origin_stop']['name_tc']}</Text>
+        <View style={[styles.column_container, styles.minute_container]}>
+          <Text style={styles.minute}>{getEtaMinuteByDate(data['eta'])}</Text>
+          <Text style={styles.minute_label}>{t('minute_label')}</Text>
+        </View>
       </View>
-      <View style={[styles.column_container, styles.minute_container]}>
-        <Text style={styles.minute}>{getEtaMinuteByDate(data['eta'])}</Text>
-        <Text style={styles.minute_label}>分鐘</Text>
-      </View>
-    </View>
-  </TouchableOpacity>
-)
+    </TouchableOpacity>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    flexWrap: 'wrap',
+    flexWrap: 'nowrap',
+    maxWidth: '100%',
     padding: 8,
     paddingHorizontal: 20,
     alignItems: 'center',
@@ -36,7 +47,7 @@ const styles = StyleSheet.create({
   },
   column_container: {
     flexDirection: 'column',
-    flexWrap: 'wrap',
+    flexWrap: 'wrap'
   },
   route: {
     fontSize: 28,
@@ -44,12 +55,14 @@ const styles = StyleSheet.create({
     width: 90,
   },
   destination_container: {
-    flex: 1,
+    flexGrow: 1,
+    flexShrink: 1,
+    width: '100%'
   },
   destination: {
     fontSize: 26,
     fontWeight: 'bold',
-    paddingHorizontal: 4,
+    paddingHorizontal: 4
   },
   destination_label: {
     fontSize: 12,
@@ -57,6 +70,7 @@ const styles = StyleSheet.create({
   },
   origin_stop: {
     fontSize: 16,
+    width: '100%'
   },
   minute_container: {
     width: 50,
